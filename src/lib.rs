3b7129fc4d4562855a776a5d53198c0cd99d97d3
user <s3420052@yandex.ru>
2023-11-05 18:13:46 +0300

    migrate to blocking reqwest

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use chrono;
 use regex::Regex;
-use reqwest::{Client, Error};
+use reqwest::{blocking::Client, Error};
 use std::{thread, time};
 
 pub struct Camera {
@@ -17,30 +17,72 @@ pub struct Car {
 }
 
 impl Camera {
-    /// return base_usl, example: http://192.168.1.1/cgi-bin
-    async fn base_url(&self) -> String {
-        let url = format!("http://{}/cgi-bin", &self.ip);
-        url
-    }
-
-    /// Get all numbers from camera in raw camera format four lines for one
-    /// car number.
+    /// Get all numbers from camera cars numbers.
+    ///
     /// Number111=carNumber
     /// Begin111=2020-12-22
     /// End111=2020-12-22
     /// Notify111=on | off
     /// 111 - sequence number in camera
-    ///
-    /// GET request
-    /// http://192.168.1.1/cgi-bin/lnpr_cgi?action=list
-    ///
-    /// return camera vector parsed response list
-    pub async fn list_numbers(&self) -> Result<Vec<Car>, Error> {
-        let base_url = &self.base_url().await;
-        let request_url = format!("{base_url}/lnpr_cgi?action=list");
-        let cars_list_row = &self.get_response(&request_url).await?;
+    pub fn list_numbers(&self) -> Result<Vec<Car>, Error> {
+        let path = "/lnpr_cgi?action=list";
+        let raw_cars_list = &self.get_response(&path)?;
 
-        let strings: Vec<&str> = cars_list_row.split("\n").collect();
+        let cars_list = self.parse_raw_cars_list(&raw_cars_list)?;
+        Ok(cars_list)
+    }
+
+    /// Add number to camera
+    /// allowed symbols for number: ABCEHKMOPTXY0-9
+    /// dates format 2023-11-22
+    pub fn add(&self, car: &Car) -> Result<String, Error> {
+        let path = format!(
+            "/lnpr_cgi?action=add&Number={}&Begin={}&End={}",
+            car.number, car.begin_date, car.end_date,
+        );
+        let response = &self.get_response(&path)?;
+        Ok(response.to_string())
+    }
+
+    /// Edit number in camera
+    /// allowed symbols: ABCEHKMOPTXY0-9
+    pub fn edit(&self, car: &Car) -> Result<String, Error> {
+        let path = format!(
+            "/lnpr_cgi?action=edit&Number={}&Begin={}&End={}",
+            car.number, car.begin_date, car.end_date,
+        );
+        let response = &self.get_response(&path)?;
+        Ok(response.to_string())
+    }
+
+    /// Remove number from camera
+    /// allowed symbols: ABCEHKMOPTXY0-9
+    pub fn remove(&self, car: &Car) -> Result<String, Error> {
+        let path = format!("/lnpr_cgi?action=remove&Number={}", car.number);
+        let response = &self.get_response(&path)?;
+        Ok(response.to_string())
+    }
+
+    /// Remove all numbers by end_date
+    pub fn remove_cars(&self, mut end_date: String) -> Result<(), Error> {
+        if end_date.is_empty() {
+            end_date = chrono::offset::Local::now().format("%Y-%m-%d").to_string();
+        }
+
+        let cars = self.list_numbers()?;
+        println!("CRON JOB: удаление номеров автомобилей за текущий день");
+        for car in cars {
+            if car.end_date == end_date {
+                let _ = &self.remove(&car)?;
+                println!("Номер {} удален", car.number);
+                thread::sleep(time::Duration::from_millis(500));
+            }
+        }
+        Ok(())
+    }
+
+    fn parse_raw_cars_list(&self, raw_cars_list: &str) -> Result<Vec<Car>, Error> {
+        let strings: Vec<&str> = raw_cars_list.split("\n").collect();
         let mut cars: Vec<Car> = vec![];
 
         for line in strings {
@@ -78,71 +120,19 @@ impl Camera {
         Ok(cars)
     }
 
-    /// Add number to camera
-    /// allowed symbols for number: ABCEHKMOPTXY0-9
-    /// dates format 2023-11-22
-    pub async fn add(&self, car: &Car) -> Result<String, Error> {
-        let base_url = &self.base_url().await;
-        let url = format!(
-            "{}/lnpr_cgi?action=add&Number={}&Begin={}&End={}",
-            base_url, car.number, car.begin_date, car.end_date,
-        );
-        let response = &self.get_response(&url).await?;
-        Ok(response.to_string())
-    }
+    pub fn get_response(&self, path: &str) -> Result<String, Error> {
+        let base_path = format!("http://{}/cgi-bin", &self.ip);
+        let url = format!("{}{}", base_path, path);
 
-    /// Edit number in camera
-    /// allowed symbols: ABCEHKMOPTXY0-9
-    pub async fn edit(&self, car: &Car) -> Result<String, Error> {
-        let base_url = &self.base_url().await;
-        let url = format!(
-            "{}/lnpr_cgi?action=edit&Number={}&Begin={}&End={}",
-            base_url, car.number, car.begin_date, car.end_date,
-        );
-        let response = &self.get_response(&url).await?;
-        Ok(response.to_string())
-    }
-
-    /// Remove number from camera
-    /// allowed symbols: ABCEHKMOPTXY0-9
-    pub async fn remove(&self, car: &Car) -> Result<String, Error> {
-        let base_url = &self.base_url().await;
-        let url = format!("{}/lnpr_cgi?action=remove&Number={}", base_url, car.number);
-        let response = &self.get_response(&url).await?;
-        Ok(response.to_string())
-    }
-
-    /// Remove all numbers by end_date
-    pub async fn remove_cars(&self, mut end_date: String) -> Result<(), Error> {
-        if end_date.is_empty() {
-            end_date = chrono::offset::Local::now().format("%Y-%m-%d").to_string();
-        }
-
-        let cars = &self.list_numbers().await?;
-                println!("CRON JOB: удаление номеров автомобилей за текущий день");
-                for car in cars {
-                    if car.end_date == end_date {
-                        let _ = &self.remove(car).await?;
-                        println!("Номер {} удален", car.number);
-                        thread::sleep(time::Duration::from_millis(500));
-                    }
-                }
-        Ok(())
-    }
-
-    /// Send request to camera and get response
-    /// For request need set base_auth
-    async fn get_response(&self, url: &str) -> Result<String, Error> {
         let client = Client::new();
         let response = client
             .get(url)
             .basic_auth(&self.username, Some(&self.password))
-            .send()
-            .await?;
+            .send()?;
 
         match response.status() {
             reqwest::StatusCode::OK => {
-                return Ok(response.text().await?);
+                return Ok(response.text()?);
             }
             reqwest::StatusCode::UNAUTHORIZED => {
                 println!("Вы не авторизованы");
@@ -151,7 +141,7 @@ impl Camera {
                 panic!("Произошла ошибка");
             }
         };
-        Ok(response.text().await?)
+        Ok(response.text()?)
     }
 }
 
